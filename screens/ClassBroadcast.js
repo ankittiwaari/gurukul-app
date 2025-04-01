3dc49e5eb4262d6c432cf605080548d00654a910
Ankit Tiwari <ankittiwaari@gmail.com>
2025-04-01 18:40:55 +0530

    add class broadcast

--- screens/ClassBroadcast.js
+++ screens/ClassBroadcast.js
@@ -1,6 +1,44 @@
-import {View, Text} from 'react-native'
+import {View, FlatList, Text, ActivityIndicator} from 'react-native'
 import ProfileHeader from '../components/ProfileHeader';
 import Styles from '../utils/AppStyles';
-export default function(){
-return (<><ProfileHeader /><View style={Styles.innerApp}><Text style={Styles.headingText}>Class broadcast</Text></View></>)
+import {useEffect, useState, useLayoutEffect} from "react";
+import Api from '../utils/Api';
+import NotificationCard from "../components/NotificationCard";
+
+export default function ({navigation}) {
+    useLayoutEffect(() => {
+        navigation.setOptions({title: "Class broadcast"});
+    }, []);
+    const [isLoading, setIsLoading] = useState(true);
+    const [communications, setCommunications] = useState([]);
+
+    function fetchData() {
+        Api.get('broadcast').then((data) => {
+            const keyList = Object.keys(data);
+            const notificationList = keyList.map((key) => ({
+                title: data[key].title,
+                id: key,
+                body: data[key].body
+            }))
+            setCommunications(notificationList);
+            setIsLoading(false);
+        })
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
+    return (<>
+        <ProfileHeader/>
+        <View style={Styles.innerApp}>
+            {<FlatList
+                data={communications}
+                renderItem={(props) => <NotificationCard {...props} onPress={navigation.push}
+                                                         navTitle={"Broadcast details"}/>}
+                onRefresh={fetchData}
+                refreshing={isLoading}
+            />}
+        </View>
+    </>)
 }
